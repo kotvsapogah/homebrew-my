d22801f4256be6921e8f74a4ea8433f88b1ba7af
Oleg <a@a>
2014-10-26 01:23:57 +0300

    -

---
+++ eclipse-osx.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class EclipseOsx < Formula
+    homepage 'https://www.eclipse.org'
+    #url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz&r=1'
+    url 'https://downloads.sourceforge.net/project/synfig/releases/0.64.1/source/ETL-0.04.17.tar.gz'
+    #sha1 'b262c92b6533183640a92db6091d74765b22c0d3'
+    version 'luna-SR1'
+
+    def install
+        (prefix/"local/eclipse").install Dir["*"]
+    end
+
+end
