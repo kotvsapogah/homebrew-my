32978b4c513a678b20dfd4b9fd6d3787d2556d09
Oleg <a@a>
2014-10-26 02:55:36 +0300

    -

---
+++ eclim.rb
@@ -0,0 +1,19 @@
+require 'formula'
+
+class Eclim < Formula
+    homepage 'http://eclim.org/install.html'
+    url "http://downloads.sourceforge.net/project/eclim/eclim/2.4.0/eclim_2.4.0.tar.gz"
+    sha1 "717210349440b86946d1eaa56df8b034d7b86501"
+    version "2.4.0"
+
+    depends_on "ant"
+    depends_on "eclipse"
+
+    def install
+        system "chmod +x org.eclim/nailgun/configure"
+        system "ant", 
+            "-Declipse.home=/usr/local/opt/eclipse", 
+            "-Dvim.files=/usr/local/share/vim/vimfiles"
+    end
+
+end
--- eclipse.rb
+++ eclipse.rb
@@ -1,17 +1,19 @@
 require 'formula'
 
 class Eclipse < Formula
+    #url 'https://downloads.sourceforge.net/project/synfig/releases/0.64.1/source/ETL-0.04.17.tar.gz'
     homepage 'https://www.eclipse.org'
-    #url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz&r=1'
-
-    if true
-        url 'https://downloads.sourceforge.net/project/synfig/releases/0.64.1/source/ETL-0.04.17.tar.gz'
+    os = `uname -s`.chomp
+    if os == "Darwin"
+        url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-macosx-cocoa-x86_64.tar.gz&r=1'
+        sha1 'dfa24dcf9917cc4a5338ed105750e50e68ea64b2'
+    elsif ps == "Linux"
+        url 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz&r=1'
     end
-    #sha1 'b262c92b6533183640a92db6091d74765b22c0d3'
     version 'luna-SR1'
 
     def install
-        (prefix/"local/eclipse").install Dir["*"]
+        prefix.install Dir["*"]
     end
 
 end
