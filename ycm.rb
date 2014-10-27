55986ceaa388313b57bbb42df47e8e2c9c2f6de1
Oleg <a@a>
2014-10-27 12:48:46 +0300

    -

--- ycm.rb
+++ ycm.rb
@@ -12,6 +12,12 @@ class Ycm < Formula
     def install
         system "mkdir", "-p", "#{HOMEBREW_PREFIX}/share/vim/vimfiles/{autoload,doc,plugin,python,colors,ftdetect,ftplugin,indent,compiler,after}"
 
+        os = `uname -s`.chomp
+        if os == "Darwin"
+            ext = "dylib"
+        elsif os == "Linux"
+            ext = "so"
+        fi
         python_prefix = `#{ENV['HOME']}/packages/bin/python2 -c "import sys; print sys.prefix"`.chomp
         ohai "Python prefix: ", python_prefix
 
@@ -22,7 +28,7 @@ class Ycm < Formula
         system "cmake", "-G", "Unix Makefiles",
             "-DPATH_TO_LLVM_ROOT=#{clang_path}",
             "-DPYTHON_EXECUTABLE=#{python_prefix}/bin/python",
-            "-DPYTHON_LIBRARY=#{python_prefix}/lib/libpython2.7.dylib",
+            "-DPYTHON_LIBRARY=#{python_prefix}/lib/libpython2.7.#{ext}",
             "-DPYTHON_INCLUDE_DIR=#{python_prefix}/include/python2.7",
             "../third_party/ycmd/cpp"
         system "make", "ycm_support_libs"
