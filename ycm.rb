450fb45cf72bdc62fcb864a0563a42065adcfa6b
Oleg <a@a>
2015-07-10 19:02:51 +0300

    -

--- ycm.rb
+++ ycm.rb
@@ -5,7 +5,6 @@ class Ycm < Formula
     url "https://github.com/Valloric/YouCompleteMe", :using => :git
     version "3.6.0"
 
-    #depends_on "homebrew/dupes/zlib"
     depends_on "cmake"
     depends_on "kotvsapogah/my/llvm"
 
