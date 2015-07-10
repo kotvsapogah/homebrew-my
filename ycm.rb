cea1887f2ccb930daa604c49e7098c9628643ad3
Oleg <a@a>
2015-07-10 17:30:11 +0300

    -

--- ycm.rb
+++ ycm.rb
@@ -20,8 +20,16 @@ class Ycm < Formula
         elsif os == "Linux"
             ext = "so"
         end
-        python_prefix = `#{ENV['HOME']}/packages/bin/python2 -c "import sys; print sys.prefix"`.chomp
+        python_prefix = `python2 -c "import sys; print sys.prefix"`.chomp
+        python_bindir = `from distutils import sysconfig; print sysconfig.get_config_var("BINDIR")`.chomp
+        python_includedir = `from distutils import sysconfig; print sysconfig.get_python_inc()`.chomp
+        python_libdir = `from distutils import sysconfig; print sysconfig.get_config_var("LIBDIR")`.chomp
+        python_version = `from distutils import sysconfig; print sysconfig.get_config_var("VERSION")`.chomp
+        python_library = "#{python_libdir}/libpython#{python_version}.#{ext}" 
         ohai "Python prefix: ", python_prefix
+        ohai "Python bindir: ", python_bindir
+        ohai "Python include: ", python_includedir
+        ohai "Python library: ", python_library
 
         clang_path = Formula["llvm"].opt_prefix/"lib/llvm"
 
@@ -29,9 +37,9 @@ class Ycm < Formula
         cd "tmp_build"
         system Formula["cmake"].bin/"cmake", "-G", "Unix Makefiles",
             "-DPATH_TO_LLVM_ROOT=#{clang_path}",
-            "-DPYTHON_EXECUTABLE=#{python_prefix}/bin/python",
-            "-DPYTHON_LIBRARY=#{python_prefix}/lib/libpython2.7.#{ext}",
-            "-DPYTHON_INCLUDE_DIR=#{python_prefix}/include/python2.7",
+            "-DPYTHON_EXECUTABLE=#{python_bindir}/python",
+            "-DPYTHON_LIBRARY=#{python_library}",
+            "-DPYTHON_INCLUDE_DIR=#{python_includedir}",
             "../third_party/ycmd/cpp"
         system "make", "ycm_support_libs"
         cd "#{buildpath}"
