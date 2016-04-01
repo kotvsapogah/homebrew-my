27f39876f081f58fc16330fba38cf0f53a6c68dc
Oleg <a@a>
2016-04-01 19:24:29 +0300

    -

--- ycm.rb
+++ ycm.rb
@@ -3,16 +3,18 @@ require 'formula'
 class Ycm < Formula
     homepage "https://github.com/Valloric/YouCompleteMe"
     url "https://github.com/Valloric/YouCompleteMe", :using => :git
-    version "3.7.0"
+    version "3.8.0"
 
-    depends_on "kotvsapogah/my/llvm"
+    #depends_on "kotvsapogah/my/llvm"
+    #depends_on "llvm38"
+    depends_on "cmake" => :build
 
     def install
-        for dir in %W[
-            autoload doc plugin python
-            colors ftdetect ftplugin indent compiler after]
-            system "mkdir", "-p", "#{HOMEBREW_PREFIX}/share/vim/vimfiles/#{dir}"
-        end
+        #for dir in %W[
+            #autoload doc plugin python
+            #colors ftdetect ftplugin indent compiler after]
+            #system "mkdir", "-p", "#{HOMEBREW_PREFIX}/share/vim/vimfiles/#{dir}"
+        #end
 
         os = `uname -s`.chomp
         if os == "Darwin"
@@ -21,31 +23,34 @@ class Ycm < Formula
             ext = "so"
         end
 
-        python = "/usr/bin/python2.7"
-        python_prefix = `#{python} -c "import sys; print sys.prefix"`.chomp
-        python_includedir = `#{python} -c "from distutils import sysconfig; print sysconfig.get_python_inc()"`.chomp
-        python_libdir = `#{python} -c "from distutils import sysconfig; print sysconfig.get_config_var('LIBDIR')"`.chomp
-        python_version = `#{python} -c "from distutils import sysconfig; print sysconfig.get_config_var('VERSION')"`.chomp
-        python_library = "#{python_libdir}/libpython#{python_version}.#{ext}" 
-        ohai "Python prefix: ", python_prefix
-        ohai "Python include: ", python_includedir
-        ohai "Python library: ", python_library
+        #python = "/usr/bin/python2.7"
+        #python_prefix = `#{python} -c "import sys; print sys.prefix"`.chomp
+        #python_includedir = `#{python} -c "from distutils import sysconfig; print sysconfig.get_python_inc()"`.chomp
+        #python_libdir = `#{python} -c "from distutils import sysconfig; print sysconfig.get_config_var('LIBDIR')"`.chomp
+        #python_version = `#{python} -c "from distutils import sysconfig; print sysconfig.get_config_var('VERSION')"`.chomp
+        #python_library = "#{python_libdir}/libpython#{python_version}.#{ext}" 
+        #ohai "Python prefix: ", python_prefix
+        #ohai "Python include: ", python_includedir
+        #ohai "Python library: ", python_library
 
-        clang_path = Formula["llvm"].opt_prefix/"lib/llvm"
+        #clang_path = Formula["llvm38"].opt_prefix/"lib/llvm-3.8/lib/libclang.#{ext}"
+        
+        clang_path = var/"libclang.#{ext}"
 
         args = [
-            "-DPATH_TO_LLVM_ROOT=#{clang_path}",
-            "-DPYTHON_EXECUTABLE=#{python}",
-            "-DPYTHON_LIBRARY=#{python_library}",
-            "-DPYTHON_INCLUDE_DIR=#{python_includedir}"
+            "-DEXTERNAL_LIBCLANG_PATH=#{clang_path}"
+            #"-DPATH_TO_LLVM_ROOT=#{clang_path}"
+            #"-DPYTHON_EXECUTABLE=#{python}",
+            #"-DPYTHON_LIBRARY=#{python_library}",
+            #"-DPYTHON_INCLUDE_DIR=#{python_includedir}"
         ]
 
         mktemp do
-            system "cmake", "-G", "Unix Makefiles", buildpath/"third_party/ycmd/cpp", *args
-            system "make", "ycm_support_libs"
+            system "cmake", "-G", "Unix Makefiles", var/".vim/bundle/YouCompleteMe/third_party/ycmd/cpp", *args
+            system "cmake", "--build", ".", "--target", "ycm_core"
         end
 
-        (prefix/"share/vim/vimfiles").install Dir["*"]
+        (prefix/"ycm").install Dir["*"]
 
     end
 
